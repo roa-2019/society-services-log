00eb5df95736376b8d4f20f7077a996e6b43cadd
Student <student@pop-os.localdomain>
2019-09-19 13:25:20 +1200

    spreadoperator

--- client/components/Clients.jsx
+++ client/components/Clients.jsx
@@ -37,6 +37,7 @@ class Clients extends React.Component {
         console.log(this.state)
         this.setState({
             client: { 
+                ...this.state.client,
                 [name]: value
             }
 
@@ -73,23 +74,30 @@ class Clients extends React.Component {
                     <label><h5>First Name</h5></label>
                     <input type='text' name='firstName' onChange={this.handleChange} value={this.state.client.firstName} />
                     <br />
+
                     <label><h5>Last Name</h5></label>
                     <input type='text' name='lastName' onChange={this.handleChange} value={this.state.client.lastName} />
                     <br />
+
                     <label><h5>Address</h5></label>
                     <input type='text' name='address' onChange={this.handleChange} value={this.state.client.address} />
                     <br />
+
                     <label><h5>Brief Description of situation</h5></label>
                     <textarea name="situation" rows="10" cols="50" onChange={this.handleChange} value={this.state.client.situation} ></textarea>
                     <br />
+
                     <h3>Services provided</h3>
 
                     <label><h5>Date</h5></label>
                     <input type="date" name="service_date" onChange={this.handleChange} value={this.state.client.service_date} />
                     <br />
+
                     <label><h5>Type of Service</h5></label>
-                    <select onChange={this.handleChange} value={this.state.client.service_type}>
-                        <option value=''>...Select</option>
+                    <select placeholder="Select type of service"
+                    name="service_type"
+                    value={this.state.client.service_type} onChange={this.handleChange} >
+                        <option value='null'>...Select</option>
                         <option value='visit'>Visit</option>
                         <option value='fuelvoucher'>Fuel Voucher</option>
                         <option value='foodvoucher'>Food Voucher</option>
@@ -97,10 +105,12 @@ class Clients extends React.Component {
                         <option value='studentgrant'>Student grants</option>
                         <option value='other'>Other</option>
                     </select>
+
                     <br />
                     <label><h5>Costs incurred</h5></label>
                     $<input type="number" min="0.01" step="0.01" max="500" name="cost" onChange={this.handleChange} value={this.state.client.cost} />
                     <br />
+
                     <p>Please note: if costs incurred exceed the limit of $200 please consult Conference Leadership before continuing</p>
                     <br />
                     <label><h5>Time spent</h5></label>
@@ -109,6 +119,7 @@ class Clients extends React.Component {
                     <label><h5>Brief description of services provided</h5></label>
                     <textarea name="service_desc" rows="10" cols="50" onChange={this.handleChange} value={this.state.client.service_desc}></textarea>
                     <br />
+
                     <input type='submit' value="SAVE" />
                 </form>
             </React.Fragment>
